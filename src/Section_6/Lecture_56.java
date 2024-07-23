6c5402b104f74e1e3dcf632ac1be1fffd4e142ba
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-07-23 21:01:53 +0530

    Completed lecture 56

--- src/Section_6/Lecture_56.java
+++ src/Section_6/Lecture_56.java
@@ -1,5 +1,28 @@
 package Section_6;
 
 public class Lecture_56 {
-    // Switch Statement
+
+    public static void main(String[] args) {
+        int value = 2;
+        if (value == 1) {
+            System.out.println("Value was 1");
+        } else if (value == 2){
+            System.out.println("Value was 2");
+        } else {
+            System.out.println("Value was not 1 either 2");
+        }
+
+        // With Switch Statement
+
+        switch (value) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+            case 2:
+                System.out.println("Value was 2");
+                break;
+            default:
+                System.out.println("Value was not 1 either 2");
+        }
+    }
 }
