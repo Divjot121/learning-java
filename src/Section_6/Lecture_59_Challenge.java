79ceb876be068adaf2b81fd66841b7820d1d7125
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-07-25 16:43:04 +0530

    Solved challenge now

--- src/Section_6/Lecture_59_Challenge.java
+++ src/Section_6/Lecture_59_Challenge.java
@@ -3,6 +3,7 @@ package Section_6;
 public class Lecture_59_Challenge {
     public static void main(String[] args) {
         printDayOfWeek(7);
+        printWeekDay(1);
     }
 
     public static void printDayOfWeek(int day) {
@@ -19,6 +20,22 @@ public class Lecture_59_Challenge {
     }
 
     public static void printWeekDay(int day) {
-
+        if (day == 0 || day == 7) {
+            System.out.println("Sunday");
+        } else if (day == 1) {
+            System.out.println("Monday");
+        } else if (day == 2) {
+            System.out.println("Tuesday");
+        } else if (day == 3) {
+            System.out.println("Wednesday");
+        } else if (day == 4) {
+            System.out.println("Thursday");
+        } else if (day == 5) {
+            System.out.println("Friday");
+        } else if (day == 6) {
+            System.out.println("Saturday");
+        } else {
+            System.out.println("Invalid Argument!");
+        }
     }
 }
