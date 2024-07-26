3df594cbb621fe07a7335e77264d2e2accf12987
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-07-26 21:58:23 +0530

    for loops try

---
+++ src/Section_6/Lecture_60.java
@@ -0,0 +1,9 @@
+package Section_6;
+
+public class Lecture_60 {
+    public static void main(String[] args) {
+        for (int i = 3, j= 0; i < 10; j++) {
+            System.out.println(j);
+        }
+    }
+}
