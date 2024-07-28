1fa9ad81da24a30dec9b3cd7d14cf7ba32e59826
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-07-28 16:18:39 +0530

    GitIgnore updated

--- src/Section_6/Lecture_61_Challenge.java
+++ src/Section_6/Lecture_61_Challenge.java
@@ -1,4 +1,34 @@
 package Section_6;
 
+import java.util.Scanner;
+
 public class Lecture_61_Challenge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number you want to check");
+        int numInput = scanner.nextInt();
+        isPrime(numInput);
+    }
+
+    public static boolean isPrime(int num) {
+        boolean flag = true;
+        if (num == 0 || num == 1) {
+            System.out.println(num + " is not a prime number");
+        } else if (num == 2) {
+            System.out.println("Number " + num + " is a prime number");
+        } else {
+            for (int divisor = 2; divisor < num;) {
+                if (num % divisor == 0) {
+                    System.out.println("Number " + num + " is not a prime number");
+                    flag = false;
+                    break;
+                } else {
+                    System.out.println("Number " + num + " is a prime number");
+                    break;
+                }
+
+            }
+        }
+        return flag;
+    }
 }
