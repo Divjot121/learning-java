a64568b2ac58323479429af17eb0ae87da8cd168
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-07-29 01:06:44 +0530

    Solved Challenge

--- src/Section_6/Lecture_62_Challenge.java
+++ src/Section_6/Lecture_62_Challenge.java
@@ -20,6 +20,20 @@ package Section_6;
 
 public class Lecture_62_Challenge {
     public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
 
+        for (int i = 1; i <= 1000; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println(i);
+                count++;
+                sum += i;
+                System.out.println(sum);
+            }
+            if (count == 5) {
+                System.out.println("5 Found - Ending The Loop");
+                break;
+            }
+        }
     }
 }
