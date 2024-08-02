26f49330955ad52934d32ee123636629fb482d47
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-02 21:33:33 +0530

    pattern printing

---
+++ src/Section_6/Pattern_Printing_Loops.java
@@ -0,0 +1,15 @@
+package Section_6;
+
+public class Pattern_Printing_Loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 4; j >= i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
