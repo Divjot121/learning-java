b3f9a3246939c3bb21d8fb950368274c6feb22b1
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-03 23:02:39 +0530

    practicing patterns

--- src/Section_6/Pattern_Printing_Loops.java
+++ src/Section_6/Pattern_Printing_Loops.java
@@ -2,14 +2,11 @@ package Section_6;
 
 public class Pattern_Printing_Loops {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
-            for (int j = 4; j >= i; j--) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
+    for (int i = 4; i >= 1; i--)  { // Loop for the Row
+        for (int j = 1; j <= i; j++) { // Nested Loop for the Column
+            System.out.print(" " + i + " ");
         }
+        System.out.println();
+    }
     }
 }
