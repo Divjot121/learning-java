c438b6a8d1e2ca235f2e26a3d2bf05f2f39d905a
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-04 00:00:55 +0530

    File name changed

--- src/Section_6/Opp_Num_Pattern_Printing_Loops.java
+++ src/Section_6/Opp_Num_Pattern_Printing_Loops.java
@@ -1,6 +1,6 @@
 package Section_6;
 
-public class Pattern_Printing_Loops {
+public class Opp_Num_Pattern_Printing_Loops {
     public static void main(String[] args) {
     for (int i = 4; i >= 1; i--)  { // Loop for the Row
         for (int j = 1; j <= i; j++) { // Nested Loop for the Column
