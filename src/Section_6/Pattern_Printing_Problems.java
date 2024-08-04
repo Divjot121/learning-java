aaa9bde840adc233128a25dc10e5078af8e2f903
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-04 18:03:43 +0530

    Solved challenge 3

--- src/Section_6/Pattern_Printing_Problems.java
+++ src/Section_6/Pattern_Printing_Problems.java
@@ -15,13 +15,33 @@ public class Pattern_Printing_Problems {
 //        }
 
         //* 2nd Problem - Number Pyramid
-        int n = 5;
+//        int n = 5;
+//
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(i);
+//            }
+//            System.out.println();
+//        }
 
+        //* 3rd Problem - Palindromic Number Pyramid
+        int n = 5;
         for (int i = 1; i <= n; i++) {
+            //this loop is for the space
+            for (int j = i; j <= n; j++) {
+                System.out.print(" ");
+            }
+            //this loop is for print number 1 to i
             for (int j = 1; j <= i; j++) {
-                System.out.print(i);
+                System.out.print(j + "");
+            }
+            //this loop is for the number in reverse order
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + "");
             }
             System.out.println();
         }
+
     }
+
 }
