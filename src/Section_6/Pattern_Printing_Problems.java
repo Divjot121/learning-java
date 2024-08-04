7a2a981214c3c2514ba913e317b95c4cd121ae74
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-04 17:52:46 +0530

    Solved challenge 1

---
+++ src/Section_6/Pattern_Printing_Problems.java
@@ -0,0 +1,22 @@
+package Section_6;
+
+public class Pattern_Printing_Problems {
+    public static void main(String[] args) {
+        //* 1st Problem - Solid Rhombus
+//        int n = 5;
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 4; j >= i; j--) {
+//                System.out.print("  ");
+//            }
+//            for (int k = 1; k <= n; k++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println();
+//        }
+
+        //* 2nd Problem - Number Pyramid
+        int n = 5;
+        int m = 5;
+
+    }
+}
